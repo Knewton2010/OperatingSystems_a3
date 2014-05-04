6d56a02b6b1f19bd57ff62236598e8d7d539b95a
BenKettlewell <ben.kettlewell@gmail.com>
2014-05-03 21:05:09 -0700

    Update forking.h
    Implemented out_time_left(), output_clock(), and exit calls for child threads

--- forking.h
+++ forking.h
@@ -1,4 +1,45 @@
+
+#include <time.h>
+
 int status;
+
+void out_time_left(){
+	cout << time_length-- << endl;
+	sleep(1);
+}
+
+void output_clock(){
+
+	time_t rawtime;
+	struct tm * timeinfo;
+	do {
+		time (&rawtime);
+		timeinfo = localtime (&rawtime);
+		printf("%s", asctime(timeinfo));
+	sleep(1);
+	} while (time_length-- >0);
+}
+#include <time.h>
+
+int status;
+
+void out_time_left(){
+	cout << time_length-- << endl;
+	sleep(1);
+}
+
+void output_clock(){
+
+	time_t rawtime;
+	struct tm * timeinfo;
+	do {
+		time (&rawtime);
+		timeinfo = localtime (&rawtime);
+		printf("%s", asctime(timeinfo));
+	sleep(1);
+	} while (time_length-- >0);
+}
+
 
 void forking(){
 
@@ -16,6 +57,12 @@ void forking(){
 		else{
 			cout << "I'm the second child" << endl;
 			printf("I'm pID %d\n",getpid());
+			
+			do {
+				out_time_left();
+			} while (time_length > 0);
+			
+			exit(3);
 		}
 	}
 
@@ -23,5 +70,9 @@ void forking(){
 	else{
 		cout << "I'm the first child" << endl;
 		printf("I'm pID %d\n",getpid());
+		output_clock();
+		
+		exit (3);
+		
 	}
 }
