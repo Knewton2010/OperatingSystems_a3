913abbeb65646248e8f0662ec8b38b114279c0cc
Knewton2010 <Knewton2010@gmail.com>
2014-05-05 02:32:50 -0700

    Updated a3, Removed take_input
    Added ability to accept arguments with error checking
    Removed take_input, it's no longer used

--- a3.cc
+++ a3.cc
@@ -9,20 +9,51 @@ a3.cc */
 #include <pthread.h>
 #include <stdlib.h>
 #include <string>
+#include <sstream>
 
 using namespace std;
 
-#include "take_input.h"
+//#include "take_input.h"
 #include "forking.h"
 
-
-
-int main()
+int main(int argc, char* argv[])
 {
-	take_input();
+	time_length = 6;
+	cout << time_length << endl;
+
+
+	if (argc > 2){
+		cout << "Too many arguements." << endl;
+		std::cerr << "Usage: " << argv[0] << " Seconds" << endl;
+		return 1;
+	}
+
+	if (argc == 2){
+
+		istringstream ss(argv[1]);
+		if (!(ss >> time_length)){
+   			 cerr << "Improper Argument: " << argv[1] << '\n';
+			 cout << "Usage: a3 seconds" << endl;
+			 return 1;
+		}
+	}
+
+	/*if (argc > 2){
+		cout << "Too many arguements." << endl;
+		std::cerr << "Usage: " << argv[0] << " Seconds" << endl;
+		return 1;
+	}
+
+	if (argc == 2){
+		time_length = atoi(argv[1]);
+		cout << time_length << endl;
+	}
+	*/
+	//take_input();
 	forking();
-	
+
 	cout << "***************" << endl;
 	cout << "Program complete!" << endl;
 
 }
+
--- forking.h
+++ forking.h
@@ -2,7 +2,7 @@
 Kyle Newton
 CS570, Spring 2014
 Assignment #3, Countdown Timer
-take_input.h */
+forking.h */
 
 #include <stdio.h>
 #include <time.h>
@@ -10,12 +10,21 @@ take_input.h */
 #include <string>
 
 int status;
+int time_length;
 
 void out_time_left(){
-	cout << time_length-- << endl;
+
+	/*
+	subtract 1 from the input
+	*/
 	sleep(1);
+	cout << --time_length << endl;
 }
 
+/*
+Child 1 will call output_clock in order to output the time, then wait
+until child 2 has told it to go again via pipe
+*/
 void output_clock(){
 
 	time_t rawtime;
@@ -23,7 +32,6 @@ void output_clock(){
 		time (&rawtime);
 		timeinfo = localtime (&rawtime);
 		printf("%s", asctime(timeinfo));
-	sleep(1);
 }
 
 void forking(){
@@ -32,35 +40,43 @@ void forking(){
 	char buf;
 	string done = "blargalarg";
 	ret = pipe(mypipefd);
-	
+
 	if (fork()){
 
 		//parent
 		if (fork()){
 			cout << "I'm the parent" << endl;
 			printf("I'm pID %d\n",getpid());
+
+			/*
+			wait until both children are closed before continuing
+			*/
 			waitpid(-1, &status, 0);
 			cout << "Children successfully closed." << endl;
 		}
 
 		//child 2
 		else{
-			close(mypipefd[0]);	/* Child 2 doesn't read */
-				
+			/*
+			Child 2 doesn't read, it just writes to inform child 1 to continue
+			*/
+			close(mypipefd[0]);
+
 			cout << "I'm the second child" << endl;
 			printf("I'm pID %d\n",getpid());
-			
+
+			cout << time_length << endl;
 			do {
 				write(mypipefd[1], &done, 2);
 				out_time_left();
 			} while (time_length > 0);
-			
+
 			cout << "It's Killing TIME" << endl;
-			
+
 			close(mypipefd[1]);
-			
+
 			cout << "DEAD!" << endl;
-			
+
 			exit(3);
 		}
 	}
@@ -70,18 +86,23 @@ void forking(){
 
 		cout << "I'm the first child" << endl;
 		printf("I'm pID %d\n",getpid());
-		close(mypipefd[1]);		/* Child 1 doesn't write */
-		
-		
-		do {
+
+		/*
+		Child 1 doesn't write, it just reads from child 2 to know to continue
+		*/
+		close(mypipefd[1]);
+
+		/*
+		While child 2 continues to feed child 1 through the pipe,
+		child 1 will keep running output_clock
+		*/
+		while (read(mypipefd[0], &buf, 10)>0)
 			output_clock();
-		
-					
-		} while (read(mypipefd[0], &buf, 10)>0);
+
 		close(mypipefd[0]);
 
 		exit(3);
-		
-		
+
+
 	}
 }
