d17f725ec83fc0c55d0d309fa4dede338b4ec042
BenKettlewell <ben.kettlewell@gmail.com>
2014-05-04 22:12:59 -0700

    Added: Pipe conditional process close
    Removed automatic process ending.
    Fork method generates a pipe. 
    Child 2 writes junk data to the write end.
    Child 1 runs until there is no junk data to read.

--- forking.h
+++ forking.h
@@ -1,5 +1,13 @@
+/*
+Kyle Newton
+CS570, Spring 2014
+Assignment #3, Countdown Timer
+take_input.h */
 
+#include <stdio.h>
 #include <time.h>
+#include <unistd.h>
+#include <string>
 
 int status;
 
@@ -12,37 +20,19 @@ void output_clock(){
 
 	time_t rawtime;
 	struct tm * timeinfo;
-	do {
 		time (&rawtime);
 		timeinfo = localtime (&rawtime);
 		printf("%s", asctime(timeinfo));
 	sleep(1);
-	} while (time_length-- >0);
 }
-#include <time.h>
-
-int status;
-
-void out_time_left(){
-	cout << time_length-- << endl;
-	sleep(1);
-}
-
-void output_clock(){
-
-	time_t rawtime;
-	struct tm * timeinfo;
-	do {
-		time (&rawtime);
-		timeinfo = localtime (&rawtime);
-		printf("%s", asctime(timeinfo));
-	sleep(1);
-	} while (time_length-- >0);
-}
-
 
 void forking(){
-
+	int mypipefd[2];
+	int ret;
+	char buf;
+	string done = "blargalarg";
+	ret = pipe(mypipefd);
+	
 	if (fork()){
 
 		//parent
@@ -55,12 +45,21 @@ void forking(){
 
 		//child 2
 		else{
+			close(mypipefd[0]);	/* Child 2 doesn't read */
+				
 			cout << "I'm the second child" << endl;
 			printf("I'm pID %d\n",getpid());
 			
 			do {
+				write(mypipefd[1], &done, 2);
 				out_time_left();
 			} while (time_length > 0);
+			
+			cout << "It's Killing TIME" << endl;
+			
+			close(mypipefd[1]);
+			
+			cout << "DEAD!" << endl;
 			
 			exit(3);
 		}
@@ -68,11 +67,21 @@ void forking(){
 
 	//child 1
 	else{
+
 		cout << "I'm the first child" << endl;
 		printf("I'm pID %d\n",getpid());
-		output_clock();
+		close(mypipefd[1]);		/* Child 1 doesn't write */
 		
-		exit (3);
+		
+		do {
+			output_clock();
+		
+					
+		} while (read(mypipefd[0], &buf, 10)>0);
+		close(mypipefd[0]);
+
+		exit(3);
+		
 		
 	}
 }
